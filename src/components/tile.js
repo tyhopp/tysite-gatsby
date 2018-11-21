1e26496f5f013f5222a92c1a3fd14ce270b11dfb
Ty <thopp@polity.tech>
2018-11-20 18:33:47 -0800

    Update tile to border design language

--- src/components/tile.js
+++ src/components/tile.js
@@ -1,17 +1,15 @@
 import styled from 'styled-components'
-import { white } from '../utils/colors'
+import { white, gray } from '../utils/colors'
 
 const Tile = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	padding: 0.75em 1.5em;
+	padding: 1em 2em;
 	background-color: ${white};
 	margin: 2em 0;
-	border-radius: 10px;
 	max-width: 300px;
-	box-shadow: 0 1px 3px rgba(2, 8, 54, 0), 0 1px 7px rgba(2, 8, 54, 0.1);
-	transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+	border: 1px solid ${gray};
 `
 
 export default Tile
