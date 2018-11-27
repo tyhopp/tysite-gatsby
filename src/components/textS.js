901edf3d27565fd3d11482bf8fadcc48765908cf
Ty <thopp@polity.tech>
2018-11-27 12:11:08 -0800

    Adjust font size

--- src/components/textS.js
+++ src/components/textS.js
@@ -1,16 +1,21 @@
 import styled from 'styled-components'
+import media from '../utils/media'
 import { dark } from '../utils/colors'
 
 const TextS = styled.div`
 	font-family: ${props =>
 			props.medium ? `HK Grotesk Medium` : `HK Grotesk Regular`},
 		sans-serif;
-	font-size: 16px;
+	font-size: 17px;
 	color: ${dark};
 	line-height: 28px;
 	padding: ${props => (props.padding ? props.padding : `auto`)};
 	margin: ${props => (props.margin ? props.margin : `auto`)};
 	text-align: ${props => (props.center ? `center` : `left`)};
+
+	${media.desk`
+		font-size: 18px;
+	`}
 `
 
 export default TextS
