4f5d63b5c82925073cfcb8392b3023317756def4
Ty <thopp@polity.tech>
2018-11-27 12:15:04 -0800

    Fix error adjusting font size

--- src/components/textM.js
+++ src/components/textM.js
@@ -1,16 +1,21 @@
 import styled from 'styled-components'
+import media from '../utils/media'
 import { dark } from '../utils/colors'
 
 const TextM = styled.p`
 	font-family: ${props =>
 			props.medium ? `HK Grotesk Medium` : `HK Grotesk Regular`},
 		sans-serif;
-	font-size: 17px;
+	font-size: 18px;
 	line-height: 30px;
 	color: ${dark};
 	padding: ${props => (props.padding ? props.padding : `0.5em 0`)};
 	margin: ${props => (props.margin ? props.margin : `auto`)};
 	text-align: ${props => (props.center ? `center` : `left`)};
+
+	${media.desk`
+		font-size: 17px;
+	`}
 `
 
 export default TextM
--- src/components/textS.js
+++ src/components/textS.js
@@ -1,21 +1,16 @@
 import styled from 'styled-components'
-import media from '../utils/media'
 import { dark } from '../utils/colors'
 
 const TextS = styled.div`
 	font-family: ${props =>
 			props.medium ? `HK Grotesk Medium` : `HK Grotesk Regular`},
 		sans-serif;
-	font-size: 17px;
+	font-size: 16px;
 	color: ${dark};
 	line-height: 28px;
 	padding: ${props => (props.padding ? props.padding : `auto`)};
 	margin: ${props => (props.margin ? props.margin : `auto`)};
 	text-align: ${props => (props.center ? `center` : `left`)};
-
-	${media.desk`
-		font-size: 18px;
-	`}
 `
 
 export default TextS
