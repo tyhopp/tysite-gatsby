b89d49cd762b3efc106e47b15026b55d7279cebc
Ty <thopp@polity.tech>
2018-11-27 12:21:28 -0800

    Adjust font size again

--- src/components/textM.js
+++ src/components/textM.js
@@ -1,21 +1,16 @@
 import styled from 'styled-components'
-import media from '../utils/media'
 import { dark } from '../utils/colors'
 
 const TextM = styled.p`
 	font-family: ${props =>
 			props.medium ? `HK Grotesk Medium` : `HK Grotesk Regular`},
 		sans-serif;
-	font-size: 18px;
+	font-size: 17px;
 	line-height: 30px;
 	color: ${dark};
 	padding: ${props => (props.padding ? props.padding : `0.5em 0`)};
 	margin: ${props => (props.margin ? props.margin : `auto`)};
 	text-align: ${props => (props.center ? `center` : `left`)};
-
-	${media.desk`
-		font-size: 17px;
-	`}
 `
 
 export default TextM
