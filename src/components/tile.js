9fd3529e76ba2327482abcaedd7c3f00ba94cd9d
Ty <thopp@polity.tech>
2018-11-29 11:24:09 -0800

    Adjust tile spacing on mbl and desk

--- src/components/tile.js
+++ src/components/tile.js
@@ -1,12 +1,17 @@
 import styled from 'styled-components'
+import media from '../utils/media'
 import { white, gray } from '../utils/colors'
 
 const Tile = styled.div`
-	padding: 1em 2em;
+	padding: 1em 1.5em;
 	background-color: ${white};
 	margin: 1em;
 	max-width: 300px;
 	border: 1px solid ${gray};
+
+	${media.tab`
+		padding: 1em 3em;
+	`}
 `
 
 export default Tile
