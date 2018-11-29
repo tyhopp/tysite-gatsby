0d7f78ec821b4ee93cde0d07cac763c06dd404d3
Ty <thopp@polity.tech>
2018-11-29 11:32:16 -0800

    Add padding to tile on mbl

--- src/components/tile.js
+++ src/components/tile.js
@@ -3,7 +3,7 @@ import media from '../utils/media'
 import { white, gray } from '../utils/colors'
 
 const Tile = styled.div`
-	padding: 1em 1.5em;
+	padding: 1.5em;
 	background-color: ${white};
 	margin: 1em;
 	max-width: 300px;
