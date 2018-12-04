8b0316ce25a4b183af9bd29fd2fcb56fc424de74
Ty <thopp@polity.tech>
2018-12-04 10:30:43 -0800

    Move remark plugin to correct place in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,16 +24,16 @@ module.exports = {
 		},
 		*/
 		{
-      resolve: "gatsby-remark-external-links",
-      options: {
-        target: "_blank",
-        rel: "noopener noreferrer"
-      }
-    },
-		{
 			resolve: 'gatsby-transformer-remark',
 			options: {
 				plugins: [
+					{
+			      resolve: "gatsby-remark-external-links",
+			      options: {
+			        target: "_blank",
+			        rel: "noopener noreferrer"
+			      }
+			    },
 					{
 						resolve: 'gatsby-remark-embed-gist',
 						options: {
