7c94418281ac485610d69d7b265d9cb7782dcd78
Ty <thopp@polity.tech>
2018-12-10 09:17:40 -0800

    Remove preload fonts

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,47 +7,33 @@ import { selectFavicon } from '../utils/favicon'
 import '../utils/layout.css'
 
 class Layout extends React.Component {
-	render() {
-		const { children } = this.props
-		return (
-			<Master>
-				<Helmet>
-					<meta charSet="utf-8" />
-					<html lang="en" />
-					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-					<meta
-						name="viewport"
-						content="width=device-width, initial-scale=1.0, viewport-fit=cover"
-					/>
-					<link
-						rel="preload"
-						href="https://tyhopp.com/HKGroteskPro-Regular.woff2"
-						type="font/woff2"
-						crossorigin="anonymous"
-						as="font"
-					/>
-					<link
-						rel="preload"
-						href="https://tyhopp.com/QuincyCF-ExtraBold.woff2"
-						type="font/woff2"
-						crossorigin="anonymous"
-						as="font"
-					/>
-				</Helmet>
-				{selectFavicon(this.props.location)}
-				<Header location={this.props.location} />
-				{children}
-				{this.props.location.pathname === '/blog' ||
-				this.props.location.pathname === '/lists' ? null : (
-					<Footer />
-				)}
-			</Master>
-		)
-	}
+  render() {
+    const { children } = this.props
+    return (
+      <Master>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <html lang="en" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+          />
+        </Helmet>
+        {selectFavicon(this.props.location)}
+        <Header location={this.props.location} />
+        {children}
+        {this.props.location.pathname === '/blog' ||
+        this.props.location.pathname === '/lists' ? null : (
+          <Footer />
+        )}
+      </Master>
+    )
+  }
 }
 
 const Master = styled.div`
-	margin: 5em 0 0 0;
+  margin: 5em 0 0 0;
 `
 
 export default Layout
