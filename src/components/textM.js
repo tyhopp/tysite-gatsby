3376620be8b510bc1f684c3ffe0b3ae13d9a104f
Ty <thopp@polity.tech>
2019-03-23 16:31:15 +0800

    Reduce line height across site

--- src/components/textM.js
+++ src/components/textM.js
@@ -4,7 +4,7 @@ import { dark } from '../utils/colors'
 const TextM = styled.p`
   font-family: 'Fira Sans', sans-serif;
   font-size: 17px;
-  line-height: 32px;
+  line-height: 30px;
   color: ${dark};
   padding: ${props => (props.padding ? props.padding : `0.5em 0`)};
   margin: ${props => (props.margin ? props.margin : `auto`)};
