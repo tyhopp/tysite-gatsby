5b4ffe2609fc5fbcc093d64c12d9484c18fa5ba2
Ty Hopp <tyhopp@users.noreply.github.com>
2019-12-15 22:13:14 +0800

    Update bear-to-contentful parser

--- functions/bear-to-contentful/src/utils/format.js
+++ functions/bear-to-contentful/src/utils/format.js
@@ -3,44 +3,24 @@
  * @param {string} text the raw markdown to process
  */
 const parse = text => {
-    // console.log(typeof text);
-    // console.log(/(.*?)#/s.exec(text));
-    // console.log(/(.*?)#/s.exec(text)[1]);
-    // console.log(text.substring(/(.*?)#/s.exec(text)[1].length));
+    // Trim everything before first #
     const trimmedText = text.substring(/(.*?)#/s.exec(text)[1].length);
-    console.log(typeof trimmedText);
-    console.log(trimmedText);
-    console.log(/categories: (.*?)\n/.exec(trimmedText));
-    console.log(/categories: (.*?)\n/.exec(trimmedText)[1]);
-    console.log(/# (.*?)\n/.exec(trimmedText));
-    console.log(/# (.*?)\n/.exec(trimmedText)[1]);
-    console.log(/slug: (.*?)\n/.exec(trimmedText));
-    console.log(/slug: (.*?)\n/.exec(trimmedText)[1]);
-    console.log(/date: (.*?)\n/.exec(trimmedText));
-    console.log(/date: (.*?)\n/.exec(trimmedText)[1]);
-    console.log(/description: (.*?)\n/.exec(trimmedText));
-    console.log(/description: (.*?)\n/.exec(trimmedText)[1]);
-    console.log(trimmedText.substring(/#(.*)---/s.exec(trimmedText)));
-    console.log(trimmedText.substring(/#(.*)---/s.exec(trimmedText)[1].length + 4));
 
-    // // Trim everything before first #
-    // const trimmedText = text.substring(/(.*?)#/s.exec(text)[1].length);
-
-    // // Parse categories
-    // const parsedCategories = /categories: (.*?)\n/.exec(trimmedText)[1];
-    // const categories = /,/.test(parsedCategories)
-    //   ? parsedCategories.split(', ')
-    //   : [parsedCategories];
+    // Parse categories
+    const parsedCategories = /categories: (.*?)\\n/.exec(trimmedText)[1];
+    const categories = /,/.test(parsedCategories)
+      ? parsedCategories.split(', ')
+      : [parsedCategories];
     
-    // // Parse all other values
-    // return {
-    //   title: /# (.*?)\n/.exec(trimmedText)[1],
-    //   slug: /slug: (.*?)\n/.exec(trimmedText)[1],
-    //   date: /date: (.*?)\n/.exec(trimmedText)[1],
-    //   description: /description: (.*?)\n/.exec(trimmedText)[1],
-    //   categories,
-    //   body: trimmedText.substring(/#(.*)---/s.exec(trimmedText)[1].length + 4)
-    // };
+    // Parse all other values
+     return {
+       title: /# (.*?)\\n/.exec(trimmedText)[1],
+       slug: /slug: (.*?)\\n/.exec(trimmedText)[1],
+       date: /date: (.*?)\\n/.exec(trimmedText)[1],
+       description: /description: (.*?)\\n/.exec(trimmedText)[1],
+       categories,
+       body: trimmedText.substring(/#(.*)---/s.exec(trimmedText)[1].length + 4)
+     };
 }
 
 /**
